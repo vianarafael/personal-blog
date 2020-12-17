bbbedf3a724597b0edec6c1d95e7d38e6f041aa5
vianarafael <vianarafaelds@gmail.com>
2020-12-17 10:42:09 +0900

    deployment

---
+++ components/SiteFooter.vue
@@ -0,0 +1,20 @@
+<template>
+  <footer class="site-footer">
+    Add Social Media Links
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'SiteFooter'
+}
+</script>
+
+<style scoped>
+footer {
+  background: #eee;
+  padding: 20px 10px;
+  text-align: center;
+  transition: 0.5s ease all;
+}
+</style>
